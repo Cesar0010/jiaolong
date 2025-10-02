0113f8da783fb3b252fe7d74f5cd95b3a92ac648
zhoukuai <zk0010@sjtu.edu.cn>
2025-10-02 15:35:13 +0800

    feat:实现了串口输送RoboMaster

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "iwdg.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -93,10 +94,12 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   MX_TIM12_Init();
-  // MX_IWDG_Init();
+  MX_IWDG_Init();
+  MX_UART7_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim1);
   HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+  uint8_t tx_msg[] = "RoboMaster";
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -106,7 +109,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-
+    HAL_UART_Transmit(&huart7,tx_msg,10,1000);
+    HAL_Delay(1000);
   }
 
 }
