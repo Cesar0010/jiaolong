27bf97ea2fe0155eb48c7460673a90c0b56999c0
zhoukuai <zk0010@sjtu.edu.cn>
2025-10-03 14:05:06 +0800

    实现电机转动，并可以在keil的debug读到rx_data

---
+++ Core/Src/callback.cpp
@@ -0,0 +1,27 @@
+#include "can.h"
+#include "tim.h"
+extern CAN_RxHeaderTypeDef rx_header;
+extern CAN_TxHeaderTypeDef tx_header;
+extern uint8_t tx_data[8];
+extern uint8_t rx_data[8];
+extern uint32_t can_tx_mail_box_;
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == htim6.Instance)
+    {
+        HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data, &can_tx_mail_box_);
+    }
+}
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+    if (hcan -> Instance == CAN1)
+    {
+        HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &rx_header, rx_data);
+        if (rx_header.StdId == 0x201)
+        {
+            //Motor.canRxMsgCallback(rx_data);
+        }
+    }
+}
